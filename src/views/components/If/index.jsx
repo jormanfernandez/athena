19e3e7599a44dcb186b47617ce62b403d9d5fb0d
Jorman <jfernandez@minutentag.com>
2020-07-05 22:57:52 -0300

    State handled with connect due to double render

--- src/views/components/If/index.jsx
+++ src/views/components/If/index.jsx
@@ -1,22 +1,10 @@
-import { useState } from "react";
-
 /**
  * 
  * @param {object} props Props for the react component.
  *                        Conditions<array> Array of booleans detecting if any of its values are false
  *                        Else<React.Component> JSX Component to be rendered when a false value is in the conditions
- *                        delay<int> time for the timeout to detect if it should pass
  *                        children<function> React.Component enclosed in a function. This to prevent previous rendering from the component calling it
  */
-export function If({ Conditions, Else, delay = 200, children }) {
-  const [isChecking, setIsChecking] = useState(Conditions.includes(false));
-  const [isDelay, setIsDelay] = useState(true);
-  
-  setTimeout(() => setIsDelay(false), delay);
-
-  if (isChecking && !Conditions.includes(false)) {
-    setIsChecking(false);
-  }
-
-  return isChecking ? (isDelay ? null : Else) : children();
+export function If({ Conditions, Else, children }) {
+  return Conditions.includes(false) ? Else : children();
 }
