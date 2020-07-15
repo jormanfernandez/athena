6cd5d1a6cc98b554b481d7856d6849241eaf27af
Jorman <jfernandez@minutentag.com>
2020-07-15 13:49:53 -0300

    Refactor

--- src/util/textHelper.js
+++ src/util/textHelper.js
@@ -1,28 +1,12 @@
 import spanish from "languages/spanish.json";
 import { basePath } from "routes/paths";
 
-
-export const detectLanguage = () => {
-  const uri = window.location.href;
-  const origin = window.location.origin;
-  const base = uri.replace(origin, "").split("/")[1];
-
-  switch(base) {
-    case "es":
-      return base;
-    default:
-      window.location.href = `${window.location.origin}${basePath}`
-      return undefined;
-  }
-}
-
 const getLanguage = () => {
-  const language = detectLanguage();
-
-  switch(language) {
+  switch(basePath) {
     case "es":
     default:
-      return {...spanish};
+    return {...spanish};
+    
   }
 }
 
