6ae7b9838793be05fa10791e82489e2c526cf9c2
Jorman <jfernandez@minutentag.com>
2020-08-17 19:00:45 -0300

    Form field refactor with materialize

--- src/views/components/FormField/index.jsx
+++ src/views/components/FormField/index.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import { FormErrorField } from "views/components/FormErrorField";
 
 export const FormField = React.memo(({input, meta, ...props}) => (
-  <div>
-    <input {...input} {...props}/>
-    {<FormErrorField input={input} meta={meta}/>}
+  <div className="row">
+    <div className="col s12">
+      <div className="input-field">
+        <input {...input} {...props}/>
+        {<FormErrorField input={input} meta={meta}/>}
+      </div>
+    </div>
   </div>
 ));
