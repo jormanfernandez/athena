2a4862198df51252aef56cb52e385dd343ea0d05
Jorman <jfernandez@minutentag.com>
2020-08-17 19:29:24 -0300

    Fix submitting form forever

--- src/util/form/onFormSubmit.js
+++ src/util/form/onFormSubmit.js
@@ -1,10 +1,15 @@
 import { FORM_ERROR } from "final-form";
+import { sleep } from "util/sleep";
 
-// FIXME: Why the forms after a second submit is not returning to its initial state?
 export const onFormSubmit = operatorMethod => {
   return async values => {
-    try { 
+    try {
+      const TIME_CHECK = 100;
+      const time = +(new Date());
       await operatorMethod(values);
+      if ((+(new Date()) - time) < TIME_CHECK) {
+        await sleep(TIME_CHECK);
+      }
       return undefined;
     } catch (e) {
       const { "general_error": generalErrorMessage, ...errorMessagesToPass } = e.errorsMap;
